7aaec2ca9d84a58046a13cda0eb9e30e8412c9e7
Hello-Iam-Deve <abhijeetjoshi1304@gmail.com>
2024-03-25 15:06:00 +0530

    Add all files

---
+++ gymapp/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+
+from gymapp.models import Contact,MembershipPlan,Enrollment,Trainer,Gallery,Attendance,Arms_Workout,Back,Shoulder,Chest,Leg
+
+
+# Register your models here.
+admin.site.register(Contact)
+admin.site.register(MembershipPlan)
+admin.site.register(Enrollment)
+admin.site.register(Trainer)
+admin.site.register(Gallery)
+admin.site.register(Attendance)
+admin.site.register(Arms_Workout)
+admin.site.register(Back)
+admin.site.register(Shoulder)
+admin.site.register(Chest)
+admin.site.register(Leg)
---
+++ gymapp/views.py
@@ -0,0 +1,216 @@
+
+
+# Create your views here.
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from gymapp.models import Contact,MembershipPlan,Trainer,Enrollment,Gallery,Attendance,Arms_Workout,Back,Shoulder,Chest,Leg
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+
+
+def Home(request):
+    return render(request,"index.html")
+
+
+
+
+@login_required(login_url="/login")
+def gallery(request):
+    posts=Gallery.objects.all()
+    context={"posts":posts}
+    return render(request,"gallery.html",context)
+
+
+def about(request):
+    return render(request,"about.html")
+    
+
+
+
+
+@login_required(login_url="/login")
+def attendance(request):
+    if not request.user.is_authenticated:
+        messages.warning(request,"Please Login and Try Again")
+        return redirect('/login')
+    SelectTrainer=Trainer.objects.all()
+    context={"SelectTrainer":SelectTrainer}
+    if request.method=="POST":
+        phonenumber=request.POST.get('PhoneNumber')
+        Login=request.POST.get('logintime')
+        Logout=request.POST.get('loginout')
+        SelectWorkout=request.POST.get('workout')
+        TrainedBy=request.POST.get('trainer')
+        query=Attendance(phonenumber=phonenumber,Login=Login,Logout=Logout,SelectWorkout=SelectWorkout,TrainedBy=TrainedBy)
+        query.save()
+        messages.warning(request,"Attendace Applied Success")
+        return redirect('/attendance')
+    return render(request,"attendance.html",context)
+
+
+
+
+
+def profile(request):
+    if not request.user.is_authenticated:
+        messages.warning(request,"Please Login and Try Again")
+        return redirect('/login')
+    user_phone=request.user
+    posts=Enrollment.objects.filter(PhoneNumber=user_phone)
+    attendance=Attendance.objects.filter(phonenumber=user_phone)
+    print(posts)
+    context={"posts":posts,"attendance":attendance}
+    return render(request,"profile.html",context)
+
+
+
+def signup(request):
+    if request.method=="POST":
+        username=request.POST.get('usernumber')
+        email=request.POST.get('email')
+        pass1=request.POST.get('pass1')
+        pass2=request.POST.get('pass2')
+      
+        if len(username)>10 or len(username)<10:
+            messages.info(request,"Phone Number Must be 10 Digits")
+            return redirect('/signup')
+
+        if pass1!=pass2:
+            messages.info(request,"Password is not Matching")
+            return redirect('/signup')
+       
+        try:
+            if User.objects.get(username=username):
+                messages.warning(request,"Phone Number is Taken")
+                return redirect('/signup')
+           
+        except Exception as identifier:
+            pass
+        
+        
+        try:
+            if User.objects.get(email=email):
+                messages.warning(request,"Email is Taken")
+                return redirect('/signup')
+           
+        except Exception as identifier:
+            pass
+        
+        
+        
+        myuser=User.objects.create_user(username,email,pass1)
+        myuser.save()
+        messages.success(request,"User is Created Please Login")
+        return redirect('/login')
+        
+        
+    return render(request,"signup.html")
+
+
+
+
+def handlelogin(request):
+    if request.method=="POST":        
+        username=request.POST.get('usernumber')
+        pass1=request.POST.get('pass1')
+        myuser=authenticate(username=username,password=pass1)
+        if myuser is not None:
+            login(request,myuser)
+            messages.success(request,"Login Successful")
+            return redirect('/')
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect('/login')
+            
+        
+    return render(request,"handlelogin.html")
+
+
+def handleLogout(request):
+    logout(request)
+    messages.success(request,"Logout Success")    
+    return redirect('/login')
+@login_required(login_url="/login")
+def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('fullname')
+        email=request.POST.get('email')
+        number=request.POST.get('num')
+        desc=request.POST.get('desc')
+        myquery=Contact(name=name,email=email,phonenumber=number,description=desc)
+        myquery.save()       
+        messages.info(request,"Thanks for Contacting us we will get back you soon")
+        return redirect('/contact')
+        
+    return render(request,"contact.html")
+
+@login_required(login_url="/login")
+def enroll(request):
+    if not request.user.is_authenticated:
+        messages.warning(request,"Please Login and Try Again")
+        return redirect('/login')
+
+    Membership=MembershipPlan.objects.all()
+    SelectTrainer=Trainer.objects.all()
+    context={"Membership":Membership,"SelectTrainer":SelectTrainer}
+    if request.method=="POST":
+        FullName=request.POST.get('FullName')
+        email=request.POST.get('email')
+        gender=request.POST.get('gender')
+        PhoneNumber=request.POST.get('PhoneNumber')
+        DOB=request.POST.get('DOB')
+        member=request.POST.get('member')
+        trainer=request.POST.get('trainer')
+        reference=request.POST.get('reference')
+        address=request.POST.get('address')
+        query=Enrollment(FullName=FullName,Email=email,Gender=gender,PhoneNumber=PhoneNumber,DOB=DOB,SelectMembershipplan=member,SelectTrainer=trainer,Reference=reference,Address=address)
+        query.save()
+        messages.success(request,"Thanks For Enrollment")
+        return redirect('/join')
+
+
+
+    return render(request,"enroll.html",context)
+
+
+@login_required(login_url="/login")
+def Biceps_Triceps(request):
+    posts=Arms_Workout.objects.all()
+    context={"posts":posts}
+    return render(request,"biceps-triceps.html",context)
+
+
+
+@login_required(login_url="/login")
+def Back_workout(request):
+    posts=Back.objects.all()
+    context={"posts":posts}
+    return render(request,"Back.html",context)
+
+
+@login_required(login_url="/login")
+def shoulder_workout(request):
+    posts=Shoulder.objects.all()
+    context={"posts":posts}
+    return render(request,"shoulder.html",context)
+
+
+
+@login_required(login_url="/login")
+def Chest_workout(request):
+    posts=Chest.objects.all()
+    context={"posts":posts}
+    return render(request,"chest.html",context)
+
+
+
+@login_required(login_url="/login")
+def Leg_workout(request):
+    posts=Leg.objects.all()
+    context={"posts":posts}
+    return render(request,"leg.html",context)
